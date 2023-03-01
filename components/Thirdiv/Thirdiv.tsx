703d25baf87e86b3040681364c1ece6db490b3a9
Faizan Ali <devfaizi435@gmail.com>
2023-03-01 04:46:37 -0800

    new

--- components/Thirdiv/Thirdiv.tsx
+++ components/Thirdiv/Thirdiv.tsx
@@ -3,7 +3,7 @@ const Thirdiv = () => {
     return (
         <div>
             <div className="thirdivback">
-                <h1 className='text-center pt-5'>Featured <span style={{color:"blue"}}>Products</span></h1>
+                <h1 className='text-center pt-5'>Featured <span style={{ color: "blue" }}>Products</span></h1>
                 <div className="text-center">
                     <Image
 
@@ -12,6 +12,35 @@ const Thirdiv = () => {
                         width={100}
                         height={15}
                     />
+                </div>
+                <div className='row pt-5'>
+                    <div className='col-lg-3'>
+                        <div className='thirdivonediv'>
+                            <div className='text-center'>
+                            <Image pt-5
+
+                                src="/thirdone.jpg"
+                                alt="Picture of the author"
+                                width={250}
+                                height={270}
+                            />
+                            <h5 className='pt-5'>$33.95</h5>
+                            <p>Bade'e Al Oud</p>
+                            <p>quantity:15</p>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='col-lg-3'>
+                        <div className='thirdivonediv'>
+
+                        </div>
+
+                    </div>
+                    <div className='col-lg-3'></div>
+                    <div className='col-lg-3'></div>
+
                 </div>
 
 
