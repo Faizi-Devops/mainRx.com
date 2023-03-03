f4e94c63de71fd24e03410d52d32f856d7db69e7
Faizan Ali <devfaizi435@gmail.com>
2023-03-02 21:53:11 -0800

    fourth div start

--- components/Fourthdiv/Fourthdiv.tsx
+++ components/Fourthdiv/Fourthdiv.tsx
@@ -1,8 +1,22 @@
-const Fourthdiv = () =>{
-    return(
+import Image from 'next/image'
+const Fourthdiv = () => {
+    return (
         <div>
             <div>
-                <h2>We Appreciate Your Reviews!</h2>
+                <h2 className="text-center mt-5">We Appreciate <span style={{ color: "blue" }}>Your Reviews!</span></h2>
+            </div>
+            <div className='text-center'>
+                <Image
+                    
+                    src="/logo.jpg"
+                    alt="Picture of the author"
+                    width={100}
+                    height={15}
+                />
+
+            </div>
+            <div className='text-center mt-3'>
+                <p>Your review will help us to improve quality products, and customer services.</p>
             </div>
 
         </div>
